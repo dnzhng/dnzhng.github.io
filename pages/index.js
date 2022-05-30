6a5db5369270cbd918aba99e9e84334a2cc16592
Daniel Zhang <daniel.zhang@teespring.com>
2022-05-30 12:21:06 -0400

    add dark mode support

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ const HomePage = () => (
     </div>
     <div className="grain">
       <ul>
-        <li><a href="https://instagram.com/danzhanginthemoonlight"><i className="fab fa-instagram"></i></a></li>
-        <li><a href="https://twitter.com/dnzhng"><i className="fab fa-twitter"></i></a></li>
-        <li><a href="https://linkedin.com/in/danielyzhang"><i className="fab fa-linkedin"></i></a></li>
-        <li><a href="https://github.com/dnzhng"><i className="fab fa-github"></i></a></li>
-        <li><a href="https://phlog.co"><i className="fas fa-signature"></i></a></li>
-        <li><a href="mailto:dan@dnzhng.com"><i className="far fa-envelope"></i></a></li>
+        <li><a href="https://instagram.com/danzhanginthemoonlight"><i aria-hidden className="fab fa-instagram"></i></a></li>
+        <li><a href="https://twitter.com/dnzhng"><i aria-hidden className="fab fa-twitter"></i></a></li>
+        <li><a href="https://linkedin.com/in/danielyzhang"><i aria-hidden className="fab fa-linkedin"></i></a></li>
+        <li><a href="https://github.com/dnzhng"><i aria-hidden className="fab fa-github"></i></a></li>
+        <li><a href="https://phlog.co"><i aria-hidden className="fas fa-signature"></i></a></li>
+        <li><a href="mailto:dan@dnzhng.com"><i aria-hidden className="far fa-envelope"></i></a></li>
       </ul>
     </div>
   </Layout>
