9df80c4ee8fb0ea7d186eff8de0cc3416475c43b
Daniel Zhang <daniel.zhang@teespring.com>
2022-05-31 11:18:02 -0400

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,17 @@ const Document = () => {
       <Head>
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700i&display=swap" rel="stylesheet"/>
         <script src="https://kit.fontawesome.com/5d40daf955.js" crossOrigin="anonymous"></script>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-41808375-1"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-41808375-1');`
+          }}
+        />
       </Head>
       <body className={ darkMode ? 'dark' : '' }>
         <Main />
